9c7adbdc7f5383f94b5d0aa8d4554a1f099bcb34
Alinakbdhk <125472323+Alinakbdhk@users.noreply.github.com>
2023-02-27 21:29:29 +0300

    Add files via upload

---
+++ tileimages.py
@@ -0,0 +1,10 @@
+from utils import load_image
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png'),
+    'moneta': load_image('moneta1.png')
+}
+player_image = load_image('шели.png')
+
+tile_walls = {'wall'}
